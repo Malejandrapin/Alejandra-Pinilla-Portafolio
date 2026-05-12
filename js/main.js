c296716f0c95f7c0185e740d75c198aea44fff93
Malejandrapin <dismajus@gmail.com>
2026-05-11 21:28:44 -0500

    ruta componentes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { loadComponent } from "./loadComponents.js";
 
-loadComponent("navbar", "../componentes/navbar.html");
-loadComponent("home", "../componentes/home.html");
-loadComponent("educacion", "../componentes/education.html");
+loadComponent("navbar", "./componentes/navbar.html");
+loadComponent("home", "./componentes/home.html");
+loadComponent("educacion", "./componentes/education.html");
 
 
