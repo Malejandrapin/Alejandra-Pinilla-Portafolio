767f7c6bd64f291078015222e67a382a655fccef
Malejandrapin <dismajus@gmail.com>
2026-05-11 23:32:28 -0500

    seccion experiencia

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { loadComponent } from "./loadComponents.js";
 
 loadComponent("navbar", "./componentes/navbar.html");
 loadComponent("home", "./componentes/home.html");
-loadComponent("educacion", "./componentes/education.html");
+loadComponent("education", "./componentes/education.html");
+loadComponent("experiencie", "./componentes/experience.html");
 
 
