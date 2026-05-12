e3d1392b75eb7ed891fc866462f292b382cfe8c4
Malejandrapin <dismajus@gmail.com>
2026-05-11 21:18:20 -0500

    Estructura por componentes seccion home y education

---
+++ js/loadComponents.js
@@ -0,0 +1,8 @@
+export async function loadComponent(id, file) {
+
+  const response = await fetch(file);
+
+  const html = await response.text();
+
+  document.getElementById(id).innerHTML = html;
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { loadComponent } from "./loadComponents.js";
+
+loadComponent("navbar", "../componentes/navbar.html");
+loadComponent("home", "../componentes/home.html");
+loadComponent("educacion", "../componentes/education.html");
+
+
